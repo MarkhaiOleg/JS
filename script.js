a44a31eaf9bbac5b2c26de9c402e9b7c3c22a8b1
Markhai Oleg <85074559+MarkhaiOleg@users.noreply.github.com>
2021-12-11 00:08:01 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-let startNumber = 1;
-let multiplier = 4;
-let quantity = 7;
+let lastNumber = 10;
+let sum = 0;
 
-for (i = 1; i <= quantity; i++) {
-  if (i === 1) {
-    console.log(startNumber)} else {
-      startNumber *= multiplier;
-      console.log (startNumber);
-      }
+for (let i = 1; i <= lastNumber; i++) {
+  sum += i;
   }
