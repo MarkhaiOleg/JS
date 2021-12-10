a4da0ef81c8c0ee5cce296374fda2e97a9e4fac1
Markhai Oleg <85074559+MarkhaiOleg@users.noreply.github.com>
2021-12-10 23:51:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let startNumber = 1;
+let multiplier = 4;
+let quantity = 7;
+
+for (i = 1; i <= quantity; i++) {
+  if (i === 1) {
+    console.log(startNumber)} else {
+      startNumber *= multiplier;
+      console.log (startNumber);
+      }
+  }
