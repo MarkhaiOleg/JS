cf195cc8bc0b0360225394b084fcdf49f96c5568
Markhai Oleg <85074559+MarkhaiOleg@users.noreply.github.com>
2021-12-11 00:38:55 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let lastNumber = 5;
-let multiplicationResult = 1;
-let sum = 0;
+let number = 15;
 
-for (i = 1; i <= lastNumber; i++){
-  if (i % 2 === 0) {
-    multiplicationResult *= i;
+for (i = 1; i <= number; i++) {
+  if (number % i === 0 ) {
+    if(i > 1) {
+      if(i < number) {
+    console.log (i);
     }
-  
-  
+    }
+    }
   }
