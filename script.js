40047923b748bca55b0230f169e8b68d49f1ee93
Markhai Oleg <85074559+MarkhaiOleg@users.noreply.github.com>
2021-12-11 13:36:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-var number = 23515625621;
-var cutNumber = number;
-var quantity = 0;
-var i = 1;
+let days = 9; // Дней в периоде
+let period = 3; // Как часто я ем протеин (раз в три дня)
+let workDayAmount = 200; // Количество протеина в будние
+let weekendAmount = 100; // Количество протеина в выходные
+let total = 0;
 
-while (Math.abs(number / i) >= 1) {
-  quantity ++;
-  i *= 10;
-  console.log(quantity);
-}
-
-console.log(quantity)
+for (let i = 1; i <= days; i++ ) {
+  if (i%period === 0) {
+    total += workDayAmount;
+  if (i%7 === 6 || i%7 === 0) {
+    total -= workDayAmount;  
+    total += weekendAmount;
+  }
+  }
+  }
