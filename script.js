60a287b8da824fb3b281b144696a8bdabc4369e6
Markhai Oleg <85074559+MarkhaiOleg@users.noreply.github.com>
2021-12-14 00:40:06 +0200

    Add files via upload
     программа, которая формирует заказ в виде строки

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Состав смузи
 
+// Состав смузи
+
 let liquids = ['вода', 'молоко', 'сок', 'чай', 'йогурт'];
 let fruits = ['киви', 'банан', 'персик', 'манго', 'груша', 'ананас'];
 let greens = ['мята', 'шпинат', 'руккола', 'петрушка', 'базилик'];
@@ -12,23 +14,4 @@ let chosenGreen = 2;
 
 // Заказ
 
-
-for (let i = 0; i <= liquids.length; i++) {
-  if (chosenLiquid -1  === i) {console.log(liquids[i]);
-  chosenLiquid = liquids[i];}
-  }
-  
-for (let j = 0; j <= fruits.length; j++) {
-  if (chosenFruit - 1   === j) {console.log(fruits[j]);
-  chosenFruit = fruits[j];}
-  }
-  
-for (let k = 0; k <= greens.length; k++) {
-  if (chosenGreen - 1  === k) {console.log(liquids[k]);
-  chosenGreen = greens[k];}
-  }
-  
- let order = 'Основа — ' + chosenLiquid + ', фрукт — ' + chosenFruit + ', зелень — ' + chosenGreen;
- 
- console.log(order);
-
+let order = 'Основа — ' + liquids[chosenLiquid-1] + ', фрукт — ' + fruits[chosenFruit-1] + ', зелень — ' + greens[chosenGreen-1];
