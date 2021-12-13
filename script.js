5cb2f51da9a357624aa0df7885fd5626296221bb
Markhai Oleg <85074559+MarkhaiOleg@users.noreply.github.com>
2021-12-13 20:20:07 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
-let usersByDay = [1, 2, 3, 4, 5, 6];
-console.log(usersByDay);
-let median;
+let expectedUsers = 1000;
 
+let usersByDay = [817, 581, 1370, 752, 1247, 681, 1120, 915, 875, 1341, 757, 610, 812, 741, 1139, 812, 638, 877, 1242, 1159, 1372, 1170, 845, 1289, 515, 1247, 769, 1261, 2805, 1201];
+
+// Рисуем график посещаемости
+keks.plot(usersByDay, expectedUsers);
+
+// Суммируем посещаемость
+let totalUsers = 0;
+for (let i = 0; i <= usersByDay.length - 1; i++) {
+  totalUsers += usersByDay[i];
+}
+
+// Рассчитываем среднее значение посещаемости
+let averageUsers = totalUsers / usersByDay.length;
+console.log('Средняя посещаемость: ' + averageUsers);
+
+if (averageUsers > expectedUsers) {
+  console.log('Посещаемость великолепна. Продолжай в том же духе!');
+} else {
+  console.log('Посещаемость так себе. Нужно поднапрячься!');
+}
+
+// Сортируем массив
+
+for (let i = 0; i <= usersByDay.length - 2; i++) {
+  let minValue = usersByDay[i];
+
+  for (let j = i + 1; j <= usersByDay.length - 1; j++) {
+    if (usersByDay[j] < minValue) {
+      minValue = usersByDay[j];
+      let swap = usersByDay[i];
+      usersByDay[i] = minValue;
+      usersByDay[j] = swap;
+    }
+  }
+}
+
+// Рассчитываем медиану
+
+let median;
 if (usersByDay.length % 2 !== 0) {
   let medianIndex = (usersByDay.length - 1) / 2;
-  console.log(medianIndex);
   median = usersByDay[medianIndex];
-  console.log(median);
-} else {let leftIndex = usersByDay.length / 2 - 1;
-        let rightIndex = usersByDay.length / 2 ;
-        console.log(leftIndex);
-        console.log(rightIndex);
-        median = (usersByDay[leftIndex] + usersByDay[rightIndex]) / 2;
-        console.log (median);
-        }
+} else {
+  let leftIndex = usersByDay.length / 2 - 1;
+  let rightIndex = usersByDay.length / 2;
+  median = (usersByDay[leftIndex] + usersByDay[rightIndex]) / 2;
+}
+
+console.log('Медианная посещаемость: ' + median);
