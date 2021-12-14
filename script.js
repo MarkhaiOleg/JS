d3de427584f4d1d3ad7bbdb035b88c99dabd4a51
Markhai Oleg <85074559+MarkhaiOleg@users.noreply.github.com>
2021-12-15 01:02:35 +0200

    Add files via upload
    программа, которая на основе одного массива с книгами создаст другой.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let luggage = ['пакет', 'мяч', 'тапки', 'когтеточка', 'коробка', 'миска', 'мята'];
-let startIndex = 2;
-let quantity = 3;
-let chosenLuggage = [];
+let books = ['Улисс', 'Маугли', 'Сияние', 'Ревизор', 'Гамлет', 'Обломов', 'Дюймовочка', 'Шантарам', 'Вий', 'Сильмариллион', 'Оно'];
+let minNumber = 3;
+let maxNumber = 5;
+let filteredBooks = [];
 
-
-for (let i = startIndex ; i < startIndex + quantity;i++) {
-  
-  chosenLuggage.push(luggage[i]);
+for (let i = 0; i < books.length;i++) {
+  if ( books[i].length <= maxNumber && books[i].length >= minNumber) {
+    filteredBooks.push(books[i]);
+    }
   }
-  
-console.log(chosenLuggage);
+
+console.log(filteredBooks);
