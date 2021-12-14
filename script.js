658b9ae273190fb15d953120ba469e799922d0be
Markhai Oleg <85074559+MarkhaiOleg@users.noreply.github.com>
2021-12-15 00:48:47 +0200

    Add files via upload
    программа, которая на основе одного массива c багажом создаст другой.

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-let numbers = [2, 4, 7, 4, 7, 2];
-let number = 4;
+let luggage = ['пакет', 'мяч', 'тапки', 'когтеточка', 'коробка', 'миска', 'мята'];
+let startIndex = 2;
+let quantity = 3;
+let chosenLuggage = [];
 
-let lastIndex = numbers.lastIndexOf(number);
+
+for (let i = startIndex ; i < startIndex + quantity;i++) {
+  
+  chosenLuggage.push(luggage[i]);
+  }
+  
+console.log(chosenLuggage);
