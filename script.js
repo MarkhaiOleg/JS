459d525ec79186293f2f35b6e182e2d7620bdcf2
Markhai Oleg <85074559+MarkhaiOleg@users.noreply.github.com>
2021-12-15 23:25:46 +0200

    Add files via upload
     программа, которая будет следить за спортивными показателями.

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-let fibonacciNumbers = [1, 1];
-let numbersQuantity = 7;
-let total = fibonacciNumbers.length + numbersQuantity;
+let indicators = ['сила', 'гибкость', 'выносливость', 'скорость', 'ловкость'];
+let levels = [8, 15, 9, 12, 11];
+let trainingTime = 30;
 
-for (let i = 2;i < total;i++) {
-  fibonacciNumbers.push(fibonacciNumbers[i-1] + fibonacciNumbers [i-2]);
-  }
-  
-console.log (fibonacciNumbers);
+if (trainingTime <= 30) {
+  levels[indicators.indexOf('ловкость')] += 3;
+  levels[indicators.indexOf('скорость')] += 3;
+  } 
+  else if (trainingTime > 30 && trainingTime <= 60) {
+    levels[indicators.indexOf('гибкость')] += 3;
+    }
+  else if (trainingTime > 60) {
+    levels[indicators.indexOf('сила')] += 2;
+    levels[indicators.indexOf('выносливость')] += 2;
+    }
+    
+  console.log(levels);
