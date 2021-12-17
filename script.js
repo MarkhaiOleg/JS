913f0712a99118f9154e93c4b32f649ec6b48390
Markhai Oleg <85074559+MarkhaiOleg@users.noreply.github.com>
2021-12-17 20:26:36 +0200

    Add files via upload
    бонусные мили

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-let indicators = ['сила', 'гибкость', 'выносливость', 'скорость', 'ловкость'];
-let levels = [8, 15, 9, 12, 11];
-let trainingTime = 30;
+let calculateMiles = function (distance) {
+  let percent = 0.25;
+  if (distance > 10500) {
+    percent = 0.35;
+  }
+  let miles = Math.floor(distance * percent);
+  return miles;
+};
 
-if (trainingTime <= 30) {
-  levels[indicators.indexOf('ловкость')] += 3;
-  levels[indicators.indexOf('скорость')] += 3;
-  } 
-  else if (trainingTime > 30 && trainingTime <= 60) {
-    levels[indicators.indexOf('гибкость')] += 3;
-    }
-  else if (trainingTime > 60) {
-    levels[indicators.indexOf('сила')] += 2;
-    levels[indicators.indexOf('выносливость')] += 2;
-    }
-    
-  console.log(levels);
+console.log('За перелёт в Иркутск получим ' + calculateMiles(4125) + ' миль');
+
+console.log('За перелёт на Камчатку получим ' + calculateMiles(11000) + ' миль');
