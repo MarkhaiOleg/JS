203068ce192fbbeff56d4caa995b46267024a279
Markhai Oleg <85074559+MarkhaiOleg@users.noreply.github.com>
2021-12-18 12:26:36 +0200

    Add files via upload
     программа, которая определяет, какой проект выгодней: срочный или обычный.

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 let getPrice = function(time, fast) {
   let pricePerHour = 1500;
-   
-    
+
   if (fast) {
     pricePerHour *= 2.5;
     time /= 2;
     } ;
   
-    
    if ( time > 150) {
     pricePerHour -= 250 ;
     };
-    
- 
-    
-  return pricePerHour * time;
+  let price = pricePerHour * time;
+  return price;
   }
   
-getPrice(100, true);
+let getProfitableProject = function(hours, profit) {
+  let urgentProject = getPrice(hours, true) - profit;
+  let notUrgentProject = getPrice(hours, false);
+  let value;
+  let expences;
+  
+  if (urgentProject < notUrgentProject) {
+    value = 'срочный';
+    expences = urgentProject;
+    } else {
+    value = 'обычный';
+    expences = notUrgentProject;
+    }
+    
+    return 'Выгодней ' + value + ' проект. Потратишь на него '+ expences;
+  };
+
+  
+  
+  getProfitableProject(100, 2000);
+
