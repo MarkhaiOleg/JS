a690db5834f032c4bf1d96a1e774298febb4b680
Markhai Oleg <85074559+MarkhaiOleg@users.noreply.github.com>
2021-12-18 20:08:54 +0200

    Add files via upload
     программа, которая будет анализировать мой суточный рацион.

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-let calculate = (firstNumber, secondNumber, operator) =>
-  eval(firstNumber+operator+secondNumber);
+let getDiet = function(food, limit) {
+  let total = 0;
+  for (i= 0;i <= food.length;i++) {
+    if (total > limit) {
+     return i - 1;
+   }
+   total += food[i]; 
+  }
+    
+  };
