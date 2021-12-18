8358ddd7d0032a5ffe8878b73ba437f8ca6cde81
Markhai Oleg <85074559+MarkhaiOleg@users.noreply.github.com>
2021-12-18 12:33:49 +0200

    Add files via upload
    программа для определения типа транспорта на велосипедной парковке.

--- script.js
+++ script.js
@@ -1,36 +1,5 @@
-let getPrice = function(time, fast) {
-  let pricePerHour = 1500;
-
-  if (fast) {
-    pricePerHour *= 2.5;
-    time /= 2;
-    } ;
-  
-   if ( time > 150) {
-    pricePerHour -= 250 ;
-    };
-  let price = pricePerHour * time;
-  return price;
-  }
-  
-let getProfitableProject = function(hours, profit) {
-  let urgentProject = getPrice(hours, true) - profit;
-  let notUrgentProject = getPrice(hours, false);
-  let value;
-  let expences;
-  
-  if (urgentProject < notUrgentProject) {
-    value = 'срочный';
-    expences = urgentProject;
-    } else {
-    value = 'обычный';
-    expences = notUrgentProject;
-    }
-    
-    return 'Выгодней ' + value + ' проект. Потратишь на него '+ expences;
+let checkVehicle = function(numberOfWheels, weight) {
+  if (numberOfWheels < 4 && weight < 100) {
+     return 'Парковка разрешена';
+    } else {return 'Вам здесь не место! Мяу!'};
   };
-
-  
-  
-  getProfitableProject(100, 2000);
-
