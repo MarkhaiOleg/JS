881b984c37dede155912e40e349c6c406785f573
Markhai Oleg <85074559+MarkhaiOleg@users.noreply.github.com>
2021-12-18 11:01:43 +0200

    Add files via upload
    программа для расчета цен на проект

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
-let calculateMiles = function (distance, isBusinessClass) {
-  let percent = 0.18;
-  if (isBusinessClass) {
-    percent += 0.04;
+let getPrice = function(time, fast) {
+  let pricePerHour = 1500;
+   
+    
+  if (fast) {
+    pricePerHour *= 2.5;
+    time /= 2;
+    } ;
+  
+    
+   if ( time > 150) {
+    pricePerHour -= 250 ;
+    };
+    
+ 
+    
+  return pricePerHour * time;
   }
-  if (distance > 3500) {
-    percent += 0.15;
-  }
-  return distance * percent;
-};
-
-let calculateFlights = function (distance, isBusinessClass, milesTarget) {
-  let miles = calculateMiles(distance, isBusinessClass);
-  let flights = Math.ceil(milesTarget / miles);
-  return flights;
-};
-
-let targets =[3000, 7500, 15000];
-for ( let i = 0; i<=targets.length-1; i++) {
-  let flightsVariantFirst = calculateFlights(3118, true, targets [i]);
-let flightsVariantSecond = calculateFlights(3617, false, targets [i]);
-
-console.log('Необходимое количество полётов в бизнес-классе до Валенсии: ' + flightsVariantFirst);
-console.log('Необходимое количество полётов в экономе до Лиссабона: ' + flightsVariantSecond);
-
-if (flightsVariantFirst > flightsVariantSecond) {
-  console.log('Быстрей накопишь полётами в экономе до Лиссабона! Количество полётов: ' + flightsVariantSecond);
-} else {
-  console.log('Быстрей накопишь полётами в бизнесе до Валенсии! Количество полётов: ' + flightsVariantFirst);
-}
-
-  }
+  
+getPrice(100, true);
