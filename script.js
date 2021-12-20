dccf2f265321aa07e7862586e140d567e3541337
Markhai Oleg <85074559+MarkhaiOleg@users.noreply.github.com>
2021-12-20 20:12:05 +0200

    Add files via upload
    JS в браузере

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
-let getDiet = function(food, limit) {
-  let total = 0;
-  for (i= 0;i <= food.length;i++) {
-    if (total > limit) {
-     return i - 1;
-   }
-   total += food[i]; 
+var catalogData = [
+  {
+    isAvailable: true,
+    isSpecial: false
+  },
+  {
+    isAvailable: false,
+    isSpecial: false
+  },
+  {
+    isAvailable: true,
+    isSpecial: true
+  },
+  {
+    isAvailable: true,
+    isSpecial: false
+  },
+  {
+    isAvailable: false,
+    isSpecial: false
   }
-    
-  };
+];
+
+var updateCards = function (products) {
+  var elements = document.querySelectorAll('.product');
+  for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+    console.log(element);
+    var product = products[i];
+    console.log(product);
+    var availabilityClass = 'product--available';
+    if (!product.isAvailable) {
+      availabilityClass = 'product--unavailable';      
+    }
+    element.classList.add(availabilityClass);
+  }
+};
+
+updateCards(catalogData);
