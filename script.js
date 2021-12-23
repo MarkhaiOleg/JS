d70ee097b98eec8da27a4173ca2a777a66f14a28
Markhai Oleg <85074559+MarkhaiOleg@users.noreply.github.com>
2021-12-23 20:45:27 +0200

    Add files via upload
    программа сколько балов получит студент

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-let squareOfSum = function(a,b){
-return (a * a) + 2 * a * b + (b * b);
+// @ts-check
+let finalGrade = function (exam, projects){
+if (exam> 90 || projects > 10) {
+  return 100;
+} else if (exam > 75 && projects >= 5) {
+  return 90;
+} else if (exam > 50 && 2 <= projects) {
+  return 75;
+} else {
+  return 0;
+}
 
 }
 // BEGIN (write your solution here)
 
 // END
 
-export default squareOfSum;
+export default finalGrade;
