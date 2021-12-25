91a0469a1d5dabe9959f42a1e8ce66bbc3fb073e
Markhai Oleg <85074559+MarkhaiOleg@users.noreply.github.com>
2021-12-25 19:23:04 +0200

    Add files via upload
    сумма чисел через рекурсию

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-// @ts-check
-let finalGrade = function (exam, projects){
-if (exam> 90 || projects > 10) {
-  return 100;
-} else if (exam > 75 && projects >= 5) {
-  return 90;
-} else if (exam > 50 && 2 <= projects) {
-  return 75;
-} else {
-  return 0;
-}
+const sequenceSum = (begin, end) => {
+  // BEGIN (write your solution here)
+  if (begin > end) {
+    return NaN;
+  }
+  if (begin === end) {
+    return begin;
+  }
+  return begin + sequenceSum(begin + 1, end);
+  // END
+};
 
-}
-// BEGIN (write your solution here)
-
-// END
-
-export default finalGrade;
+export default sequenceSum;
