9e1ae86c632b09a17f9a9c5b434b90c1c0e72933
Markhai Oleg <85074559+MarkhaiOleg@users.noreply.github.com>
2022-01-03 23:36:40 +0200

    Add files via upload
    событие 
    открытие и закрытие всплывающего окна с помощью кнопок интерфейса и кнопки ESC

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-var popup = document.querySelector('.modal');
-var openPopupButton = document.querySelector('.button-open');
-var closePopupButton = popup.querySelector('.button-close');
+var popup = document.querySelector('.popup');
+var openPopupButoon = document.querySelector('.button-show');
+var closePopupButoon = popup.querySelector('.button-hide');
 
-openPopupButton.addEventListener('click', function (evt) {
+openPopupButoon.addEventListener('click', function(evt) {
   evt.preventDefault();
-  popup.classList.add('modal--show');
-});
-
-closePopupButton.addEventListener('click', function () {
-  popup.classList.remove('modal--show');
-});
-
-document.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === 27) {
-  popup.classList.remove('modal--show');
+  popup.classList.add('popup--open');
+  });
+  
+closePopupButoon.addEventListener('click', function() {
+    popup.classList.remove('popup--open');
+  });
+  
+document.addEventListener('keydown', function(evt){
+  if(evt.keyCode === 27) {
+  popup.classList.remove('popup--open');
   }
-});
+  })
