cd5e067b309b4e9eb5e2c1508ec1c603fbe3b78a
Markhai Oleg <85074559+MarkhaiOleg@users.noreply.github.com>
2022-01-03 23:20:23 +0200

    Add files via upload
    событие 
    открытие и закрытие окна через кнопки в интерфейсе и ESC

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-var makeElement = function (tagName,className,text) {
-  var element = document.createElement(tagName);
-  element.classList.add(className);
-  if(text) {
-    element.textContent = text;
+var popup = document.querySelector('.modal');
+var openPopupButton = document.querySelector('.button-open');
+var closePopupButton = popup.querySelector('.button-close');
+
+openPopupButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popup.classList.add('modal--show');
+});
+
+closePopupButton.addEventListener('click', function () {
+  popup.classList.remove('modal--show');
+});
+
+document.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+  popup.classList.remove('modal--show');
   }
-  return element;
-  };
+});
