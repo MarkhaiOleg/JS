bfdd27c2cb47f5b158cfd49e3bda9daea0cf5dbc
Markhai Oleg <85074559+MarkhaiOleg@users.noreply.github.com>
2022-01-05 19:51:22 +0200

    Add files via upload
    приложение — список дел

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
-// Контейнер для карточек
-var pool = document.querySelector('.pool');
+var list = document.querySelector('.todo-list');
+var items = list.children;
+var emptyListMessage = document.querySelector('.empty-tasks');
+var newItemForm = document.querySelector('.add-form');
+var newItemTitle = newItemForm.querySelector('.add-form-input');
+var taskTemplate = document.querySelector('#task-template').content;
+var newItemTemplate = taskTemplate.querySelector('.todo-list-item');
 
-// Получаем шаблон карточки
-var template = document.querySelector('#element-template').content;
-var element = template.querySelector('div');
+var toggleEmptyListMessage = function () {
+  if (items.length === 0) {
+    emptyListMessage.classList.remove('hidden');
+  } else {
+    emptyListMessage.classList.add('hidden');
+  }
+};
 
-// Клонируем и наполняем элементы в цикле
+var addCheckHandler = function (item) {
+  var checkbox = item.querySelector('.todo-list-input');
+  checkbox.addEventListener('change', function () {
+    item.remove();
+    toggleEmptyListMessage();
+  });
+};
 
-for (var i = 1; i <= 10; i++) {
-   var clonedElement = element.cloneNode(true);
-   clonedElement.children[0].textContent = i;
-   pool.appendChild(clonedElement);
- }
+for (var i = 0; i < items.length; i++) {
+  addCheckHandler(items[i]);
+}
+
+newItemForm.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+
+  var taskText = newItemTitle.value;
+  var task = newItemTemplate.cloneNode(true);
+  var taskDescription = task.querySelector('span');
+  taskDescription.textContent = taskText;
+  addCheckHandler(task);
+
+  list.appendChild(task);
+  toggleEmptyListMessage();
+  newItemTitle.value = '';
+});
